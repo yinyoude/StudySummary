2391005321b160672c668477c411f4d2761bd154
cony <1179880488@qq.com>
2019-09-10 20:39:00 +0800

    with compile

--- HTML+CSS/vue双向绑定原理/bindVue/js/index.js
+++ HTML+CSS/vue双向绑定原理/bindVue/js/index.js
@@ -1,9 +1,45 @@
-function SelfVue (data, el, exp) {
+/* function SelfVue (data, el, exp) {
+    var self = this;
     this.data = data;
+    
+    Object.keys(data).forEach(function(key) {
+        self.proxyKey(key);     // 绑定代理属性
+    })
+
     observe(data);
-    el.innerHTML = this.data[exp];
+    el.innerHTML= this.data[exp];
     new Watcher(this, exp, function(value) {
-        el.innerHTML = value
-    });
+        el.innerHTML = value;
+    })
     return this;
+} */
+
+function SelfVue (options) {
+    var self = this;
+    this.vm = this;
+    this.data = options.data;
+
+    Object.keys(this.data).forEach(function(key) {
+        self.proxyKeys(key);
+    });
+
+    observe(this.data);
+    new Compile(options.el, this.vm);
+    return this;
+}
+
+SelfVue.prototype = {
+    proxyKeys: function(key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function getter() {
+                return self.data[key];
+            },
+            set: function setter(newVal) {
+                self.data[key] = newVal;
+            }
+        })
+    }
 }
